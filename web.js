c39ccadf6147665c5153feaad5a2937a778d2896
jdubb7 <jww1117@gmail.com>
2013-07-17 02:01:01 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buffer = new Buffer("utf-8");
   var fs = require('fs');
-  response.send(fs.readFileSync("index.html"), "utf-8");
+  response.send(fs.readFileSync("index.html"), function (err, buffer));
+  if (err) throw err;
 });
 
 var port = process.env.PORT || 5000;
