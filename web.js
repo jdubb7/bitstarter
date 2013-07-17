d268a77392832665b41ef35dcd682bf84649261b
jdubb7 <jww1117@gmail.com>
2013-07-17 02:27:32 +0000

    update web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = new Buffer("utf-8");
+  var buffer = new Buffer('utf-8');
   response.send(fs.readFileSync('index.html'), function(err, data);
   if(err) throw err;
 });
